47a0e02b81fbcbc925639f6dff60dbced8d32f7e
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2023-03-25 21:21:04 +0800

    Add MoviePoster

---
+++ src/components/MoviePoster.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import { POSTER_IMAGE_ENDPOINT } from "~/constants";
+import { Movie as MoviePoster } from "~/types";
+
+const MoviePoster = ({ ...movie }: MoviePoster) => {
+  return (
+    <>
+      <button
+        onClick={() => null}
+        className="relative aspect-[342/513] w-48 rounded border border-gray-500 shadow transition hover:border-green-500"
+      >
+        <Image
+          className="relative rounded"
+          src={`${POSTER_IMAGE_ENDPOINT}${movie.poster_path ?? ""}`}
+          alt={`${movie.title} poster`}
+          width={342}
+          height={513}
+        />
+      </button>
+    </>
+  );
+};
+
+export default MoviePoster;
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const BACKDROP_IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/w1280";
+export const POSTER_IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/w342";
+export const PROFILE_IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/w185/";
